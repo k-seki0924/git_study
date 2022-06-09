17375e12386a8c12f263df11fe0ba1da284f1498
seki <12fuseki@gmail.com>
2022-06-09 14:02:51 +0900

    ２回目のコミット

--- study2.php
+++ study2.php
@@ -1,5 +1,4 @@
 <?php
-$result = 2 + 4 - 5;
 ?>
 <!DOCTYPE html>
 <html lang="ja">
--- study3.php
+++ study3.php
@@ -1,5 +1,7 @@
 <?php
 $result = 2 * 4 - 5;
+$result = 2 * 4 - 5;
+$result = 2 * 4 - 5;
 ?>
 <!DOCTYPE html>
 <html lang="ja">
